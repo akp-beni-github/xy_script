3252fddb86eca43a4a2eb516e92a755f639b5d7e
Ben <bengzio111@gmail.com>
2024-09-04 17:41:56 +0700

    using regex for input parsing to array

--- xy_script.sh
+++ xy_script.sh
@@ -1,77 +1,92 @@
 #!/bin/bash
 
-# initial variables
-x=0
-y=0
-direction="North"
-
-# functions
+#functions
 update_direction() {
-  local turn=$1
-  case $direction in
-    "North")
-      if [ "$turn" == "R" ]; then
-        direction="East"
-      else
-        direction="West"
-      fi
-      ;;
-    "East")
-      if [ "$turn" == "R" ]; then
-        direction="South"
-      else
-        direction="North"
-      fi
-      ;;
-    "South")
-      if [ "$turn" == "R" ]; then
-        direction="West"
-      else
-        direction="East"
-      fi
-      ;;
-    "West")
-      if [ "$turn" == "R" ]; then
-        direction="North"
-      else
-        direction="South"
-      fi
-      ;;
-  esac
+    local turn=$1
+    case $dir in
+        "North")
+        if [[ "$turn" == "R" ]]; then 
+            dir="East"
+        else
+            dir="West"
+        fi
+        ;;
+        "East")
+        if [[ "$turn" == "R" ]]; then 
+            dir="South"
+        else
+            dir="North"
+        fi
+        ;;
+        "South")
+        if [[ "$turn" == "R" ]]; then 
+            dir="West"
+        else
+            dir="East"
+        fi
+        ;;
+        "West")
+        if [[ "$turn" == "R" ]]; then 
+            dir="North"
+        else
+            dir="South"
+        fi
+        ;;
+    esac 
 }
-
 
 move_forward() {
-  local steps=$1
-  case $direction in
+    local steps=$1
+    case $dir in
     "North")
-      y=$((y + steps))
-      ;;
+        #y=${y}+${steps}
+        y=$((y + steps))
+        ;;
     "East")
-      x=$((x + steps))
-      ;;
+        x=$((x + steps))
+        ;;
     "South")
-      y=$((y - steps))
-      ;;
-    "West")
-      x=$((x - steps))
-      ;;
-  esac
+        y=$((y - steps))
+        ;;
+    "West")  
+        x=$((x - steps))
+        ;;
+    esac
 }
 
-# read command
-read -p "Enter commands (e.g., W15 R W5 L W10): " user_input
-commands=($user_input)
+#init
+x=0
+y=0
+dir=North
 
-# main program
-for command in "${commands[@]}"; do
-  if [[ $command == R || $command == L ]]; then
-    update_direction $command
-  elif [[ $command == W* ]]; then
-    steps=${command:1}
-    move_forward $steps
-  fi
+commands=()
+
+#input 
+read -p "enter cmd: " user_input
+
+# regex precisely parsing string to array 
+while [[ $user_input =~ ([R]|[L]|W[0-9]+) ]]; do
+    #echo "${BASH_REMATCH[1]}"
+    commands+=("${BASH_REMATCH[1]}") # push to array as an array
+    #echo "${commands[@]}" 
+    user_input=${user_input#*"${BASH_REMATCH[1]}"} #clear the input string 
+    #echo "${user_input}"
 done
 
-# output 
-echo "X: $x, Y: $y, Direction: $direction "  
+#main prog
+for cmd in "${commands[@]}"; do
+    if [[ $cmd == R || $cmd == L ]]; then 
+        #echo "${cmd}"
+        update_direction $cmd
+    elif [[ $cmd == W* ]]; then 
+        steps=${cmd:1}
+        #echo "${cmd:1}"
+        move_forward $steps
+    fi    
+done
+
+# output
+echo "Final position: X = $x, Y = $y, Direction = $dir"
+
+
+
